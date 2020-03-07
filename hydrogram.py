e2f7ad9faa78980615f08f02843ea8af2d36994a
Lucas García <lucasgarciabaro@gmail.com>
2020-03-07 18:20:38 +0100

    RESETUP HYDROGRAM. Define hydrogram as a finite generator with an interval of time defined as an input parameter and the asimptote of the descending flow curve of the break_flow and the base_flow as a third of the time interval

--- hydrogram.py
+++ hydrogram.py
@@ -1,28 +1,3 @@
-# def hydrogram (f):
-#     f = float(f)
-    
-#     def _range (start, stop, step):
-#         x = start
-#         while x <= stop:
-#              yield x
-#              x += step
-    
-#     time = _range(0.0, 1.0, 1e-2)
-    
-#     def _hydrogram ():
-#         count = 0
-#         while True:
-#             yield f/count * f + 60
-#             count += 1
-
-#     return _hydrogram()
-
-
-def hydrogram (init, base):
-    count = 0
-    while True:
-        if count == 0: 
-            yield init
-        else:
-            yield count**-1/2 * init + base
-        count += 1
+def hydrogram (break_flow, base_flow, break_seconds):
+    for t in range(break_seconds):
+        yield break_flow * break_flow**(-1*t/(break_seconds*0.3)) + base_flow
