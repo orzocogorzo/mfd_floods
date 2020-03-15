ea750a6cefb264d3ee5199e9b978e3fb2c63d17e
Lucas García <lucasgarciabaro@gmail.com>
2020-03-15 18:58:42 +0100

    INCREASE ACUM AND EXCLUDE MARGINALS. Increase edge when start to accumulate flood accross the steps. In the catchments distribution discard catchments with a marginal value as a drained water that disapear.

--- mfd.py
+++ mfd.py
@@ -58,87 +58,80 @@ class MFD (Matrix):
         floods = self.zeros(self.dtm.shape)
         drafts = self.zeros(self.dtm.shape)
         speeds = self.zeros(self.dtm.shape)
+        slopes = self.zeros(self.dtm.shape)
         flood_factor = 0
         visited = dict()
-        last_alt = []
 
         def _drainpaths (rcs, next_step=dict(), queue=list(), level=0):
             try:
                 next_level = list()
+                reacheds = list()
                 for rc in rcs:
                     if rc in visited or not self.mannings[rc] or not self.dtm[rc]: continue                    
+                    
                     src_flood = floods[rc]
+                    src_speed = speeds[rc]
+                    # src_draft = drafts[rc]
+                    src_slope = slopes[rc]
 
-                    if src_flood / math.pow(self.cellsize, 2.0) < self.cellsize * 0.01:
+                    if src_flood/self.cellsize < 0.5 and src_speed/self.cellsize < 0.5:
                         # print("NOT ENOUGHT")
-                        ####
-                        # La distancia entre el inicio y el punto más bajo alcanzado crece a medida
-                        # que aumenta el área calculada. El criterio actual es más exigente al principio
-                        # que al final y no distingue entre montaña y plano. La distancia entre el punto
-                        # actual y el más bajo alcanzado ha de medirse como excesiva en base a un criterio
-                        # que
-                        ####
-                        if self.dtm[rc] - last_alt[-1] > (self.dtm[start] - last_alt[-1]) * 0.5:
-                            visited[rc] = True
-                            continue
 
                         if level == 0:
-                            floods[rc] += src_flood * flood_factor * min(1, speeds[rc] / self.cellsize)
+                            floods[rc] += src_flood * flood_factor * min(1, src_speed / self.cellsize)
                             drafts[rc] = self.get_draft(rc, floods[rc])
+                            speeds[rc] = self.get_speed(drafts[rc], self.mannings[rc], src_slope)
                         
                         next_step[rc] = True
                         continue
 
-                    slopes = self.get_slopes(rc, drafts)
+                    rc_slopes = self.get_slopes(rc, drafts)
                     not_visiteds = [True] * 9  # self.array(list(map(lambda d: tuple(d) not in visited and tuple(d) != rc, rc + self.deltas)))
-                    downslopes = self.get_downslopes(slopes, not_visiteds)
-                    upslopes = self.get_upslopes(slopes, not_visiteds)
+                    downslopes = self.get_downslopes(rc_slopes, not_visiteds)
+                    upslopes = self.get_upslopes(rc_slopes, not_visiteds)
                     under_volume = self.get_volumetries(downslopes, not_visiteds)
                     over_volume = self.get_volumetries(upslopes, not_visiteds)
 
-                    if downslopes.sum() == 0:
+                    if sum(downslopes) == 0:
                         over_flood = max(0, src_flood - over_volume.min() * 8)
                         drived_flood = 0
                         if over_flood == 0:
                             if level == 0:
-                                floods[rc] += src_flood * flood_factor * min(1, speeds[rc] / self.cellsize)
+                                floods[rc] += src_flood * flood_factor * min(1, src_speed / self.cellsize)
                                 drafts[rc] = self.get_draft(rc, floods[rc])
                                 speeds[rc] = 0
                             next_step[rc] = True
                             continue
                     else:
-                        drived_flood = min(src_flood, under_volume.sum())
+                        drived_flood = min(src_flood, sum(under_volume))
                         over_flood = src_flood - drived_flood
 
                     visited[rc] = True
                     if rc in next_step: del next_step[rc]
 
                     over_cacthments = self.where(src_flood > over_volume * 8, src_flood - over_volume * 8, 0)
-                    overfloods = over_cacthments ** 2 / (over_cacthments ** 2).sum() * over_flood if over_cacthments.sum() else self.zeros(slopes.shape)
-                    drivedfloods = downslopes ** 2 / (downslopes ** 2).sum() * drived_flood if downslopes.sum() else self.zeros(slopes.shape)
+                    overfloods = over_cacthments ** 2 / sum(over_cacthments ** 2) * over_flood if sum(over_cacthments) else self.zeros((9,))
+                    drivedfloods = downslopes ** 2 / sum(downslopes ** 2) * drived_flood if sum(downslopes) else self.zeros((9,))
                     rc_floods = overfloods + drivedfloods
-                    rc_speeds = self.get_speeds(slopes, self.get_draft(rc, src_flood), self.mannings[rc], self.log_and(rc_floods > 0, not_visiteds))
-                    # print(rc_speeds)
-                    # rc_floods = sum(rc_floods) * self.where(rc_floods > 0, rc_floods ** (rc_speeds/self.cellsize), 0) / \
-                    #     max(1e-2, sum(self.where(rc_floods > 0, rc_floods ** (rc_speeds/self.cellsize), 0)))
+                    rc_speeds = self.get_speeds(rc_slopes, drafts[rc], self.mannings[rc], self.log_and(rc_floods > 0, not_visiteds))
+                    rc_floods = sum(rc_floods) * self.where(rc_floods > 0, rc_floods ** (rc_speeds/self.cellsize), 0) / \
+                        max(1e-2, sum(self.where(rc_floods > 0, rc_floods ** (rc_speeds/self.cellsize), 0)))
 
-                    reacheds = list()
                     for i, (flood, speed) in enumerate(zip(rc_floods, rc_speeds)):
                         new_rc = tuple(rc + self.deltas[i])
-                        if flood == 0 or speed == 0 or not self.mannings[new_rc] or not self.dtm[new_rc]: continue
+                        if not self.mannings[new_rc] or not self.dtm[new_rc] or flood/self.cellsize < 1e-4 or speed/self.cellsize < 1e-4: continue
+                        slopes[new_rc] = slopes[new_rc] or rc_slopes[i] + rc_slopes[i] / 2
                         speeds[new_rc] = (speeds[new_rc] or speed + speed) / 2
-                        floods[new_rc] += (flood ** 2 / sum(rc_floods ** 2) + speed ** 2 / sum(rc_speeds ** 2)) / 2 * rc_floods.sum() * min(1, speed / self.cellsize)
+                        floods[new_rc] += flood
+                        # floods[new_rc] += (flood ** 2 / sum(rc_floods ** 2) + speed ** 2 / sum(rc_speeds ** 2)) / 2 * sum(rc_floods) * min(1, speed / self.cellsize)
                         drafts[new_rc] = self.get_draft(new_rc, floods[new_rc])
                         if new_rc not in visited:
                             if speed / self.cellsize > 1:
                                 reacheds.append(new_rc)
                             else:
                                 next_level.append(new_rc)
-
-                    if self.dtm[rc] < last_alt[-1]:
-                        last_alt.append(self.dtm[rc])
-                    if len(reacheds) > 0: _drainpaths(reacheds, next_step, queue, level + 1)
-
+                
+                if len(reacheds) > 0: queue.insert(0, reacheds)  # _drainpaths(reacheds, next_step, queue, level + 1)
                 if len(next_level) > 0: queue.append(next_level)
                 if len(queue) > 0: _drainpaths(queue.pop(0), next_step, queue, level + 1)
             except Exception:
@@ -153,7 +146,6 @@ class MFD (Matrix):
             floods[start] = break_flow
             drafts[start] = self.get_draft(start, break_flow)
             speeds[start] = self.get_speed(break_flow / self.cellsize ** 2, self.mannings[start], slope)
-            last_alt.append(self.dtm[start])
             next_step = {start: True}
             i = 0
             progress = progress_bar(break_time)
