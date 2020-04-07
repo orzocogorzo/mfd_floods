a8499ab4e1749e6b13cb82cfb1f849448baf1b53
Lucas García <lucasgarciabaro@gmail.com>
2020-04-07 14:31:33 +0200

    PACKAGE. Setup project as a python package published on PyPi.

---
+++ README.md
@@ -0,0 +1,4 @@
+# MFD Floods
+### Multiple Flow Direction Downstream Drainpaths modelization
+
+Execute test.py to test the application isolated.
--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-import setuptools
+import os
+from setuptools import setup
 
-setuptools.setup(
+def read (fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
     name="mfdfloods",
     version="0.1",
     scripts=["mfd/__init__.py"],
     author="Orzo Cogorzo",
     author_email="orzocogorzo@hotmail.com",
     description="A python script to modelate hidrologic behavior of downstream drainpaths",
+    long_description=read("README.md"),
+    license="MIT",
+    keywords="mfd drainpaths hidrology downstreams multiple flow direction",
     url="https://github.com/orzocogorzo/mfdfloods",
     requires=[
         "numpy",
@@ -14,8 +21,12 @@ setuptools.setup(
         "GDAL"
     ],
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
-        "License :: OSI approved :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Hydrology"
     ],
 )
