1645e5b9a6a519d1494403f1e1c0e7df28693a65
Lucas García <lucasgarciabaro@gmail.com>
2020-04-07 18:14:56 +0200

    Fix command line arguments interpreter

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ from osgeo import gdal
 from mfd import MFD, gtif
 
 
-def test (area, lng, lat, break_flow=120.0, base_flow=50.0, break_time=100, cellsize=5, radius=2000):
+def test (area, lng, lat, break_flow=120, base_flow=50, break_time=100, cellsize=5, radius=2000):
+    print(area, lng, lat, break_flow, base_flow, break_time, cellsize, radius)
     try:
         model = MFD(
             dtm_path="data/%s_dtm.tif" % area,
@@ -33,9 +34,9 @@ if __name__ == "__main__":
     kwargs["area"] = str(sys.argv[1])
     kwargs["lng"] = float(sys.argv[2])
     kwargs["lat"] = float(sys.argv[3])
-    if len(sys.argv) >= 5: kwargs["break_flow"] = int(sys.argv[4])
-    if len(sys.argv) >= 6: kwargs["base_flow"] = int(sys.argv[5])
-    if len(sys.argv) >= 7: kwargs["break_time"] = int(sys.argv[6])
-    if len(sys.argv) >= 8: kwargs["cellsize"] = int(sys.argv[7])
-    if len(sys.argv) >= 9: kwargs["radius"] = int(sys.argv[8])
+    if len(sys.argv) >= 4: kwargs["break_flow"] = int(sys.argv[4])
+    if len(sys.argv) >= 5: kwargs["base_flow"] = int(sys.argv[5])
+    if len(sys.argv) >= 6: kwargs["break_time"] = int(sys.argv[6])
+    if len(sys.argv) >= 7: kwargs["cellsize"] = int(sys.argv[7])
+    if len(sys.argv) >= 8: kwargs["radius"] = int(sys.argv[8])
     test(**kwargs)
