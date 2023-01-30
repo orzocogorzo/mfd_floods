1eaea84581bd599b663108fa4062821bbc872ce9
Lucas García <lucasgarciabaro@protonmail.com>
2023-01-31 00:31:08 +0100

    fix: GeoTransformFit

--- mfdfloods/geotransform.py
+++ mfdfloods/geotransform.py
@@ -2,15 +2,18 @@ class GeoTransformFit:
 
     def __init__(self, array, geotransform, ref_geotransform, nodata=None):
         self._array = array
-        self.x_delta = int((geotransform[0] - ref_geotransform[0]) / abs(geotransform[1]))
-        self.y_delta = int((geotransform[3] - ref_geotransform[3]) / abs(geotransform[5]))
+        self.x_delta = int((geotransform[0] - ref_geotransform[0]) / ref_geotransform[1])
+        self.y_delta = int((geotransform[3] - ref_geotransform[3]) / ref_geotransform[5])
         self._nodata = nodata
 
     def __getitem__(self, rc):
         try:
-            return self._array[(rc[0] + self.x_delta, rc[1] + self.y_delta)]
+            return self._array[(rc[0] - self.y_delta, rc[1] - self.x_delta)]
         except IndexError:
             return self._nodata
 
     def __setitem__(self, rc, value):
-        self._array[(rc[0] + self.x_delta, rc[1] + self.y_delta)] = value
+        try:
+            self._array[(rc[0] - self.y_delta, rc[1] - self.x_delta)] = value
+        except IndexError:
+            pass
