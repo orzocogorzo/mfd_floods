b28dc1c176e71e23adc018cc80961ef8e53d1d5c
Lucas García <lucasgarciabaro@protonmail.com>
2023-02-04 02:34:04 +0100

    fix: geotransformfit

--- mfdfloods/geotransform.py
+++ mfdfloods/geotransform.py
@@ -1,19 +1,25 @@
-class GeoTransformFit:
+from numpy.typing import NDArray
 
-    def __init__(self, array, geotransform, ref_geotransform, nodata=None):
+class GeoTransformFit:
+    def __init__(self, array: NDArray, gt: tuple, gt_ref: tuple, nodata=None):
         self._array = array
-        self.x_delta = int((geotransform[0] - ref_geotransform[0]) / ref_geotransform[1])
-        self.y_delta = int((geotransform[3] - ref_geotransform[3]) / ref_geotransform[5])
+        self.rx = gt_ref[1] / gt[1]
+        self.ry = gt_ref[5] / gt[5]
+        self.dx = round((gt_ref[0] - gt[0]) / gt_ref[1])
+        self.dy = round((gt_ref[3] - gt[3]) / gt_ref[5])
         self._nodata = nodata
 
-    def __getitem__(self, rc):
+    def proxy(self, rc: tuple) -> tuple:
+        return (round(rc[0] * self.ry) + self.dy, round(rc[1] * self.rx) + self.dx)
+
+    def __getitem__(self, rc: tuple) -> None:
         try:
-            return self._array[(rc[0] - self.y_delta, rc[1] - self.x_delta)]
+            return self._array[self.proxy(rc)]
         except IndexError:
             return self._nodata
 
-    def __setitem__(self, rc, value):
+    def __setitem__(self, rc: tuple, value: float) -> None:
         try:
-            self._array[(rc[0] - self.y_delta, rc[1] - self.x_delta)] = value
+            self._array[self.proxy(rc)] = value
         except IndexError:
             pass
