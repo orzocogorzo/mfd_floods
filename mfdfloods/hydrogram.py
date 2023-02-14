2970a0ac90ba7480ccd257d42fa83d5ba0f74fac
Lucas García <lucasgarciabaro@protonmail.com>
2023-02-14 12:09:42 +0100

    feat: hydrogram statistics

--- mfdfloods/hydrogram.py
+++ mfdfloods/hydrogram.py
@@ -1,21 +1,49 @@
 from typing import Generator
 
 
-def median_flow(hydrogram: list) -> tuple:
-    hist = {}
-    for d in hydrogram:
-        bucket = int(d[1])
-        hist[bucket] = hist.get(bucket, 0) + 1
+# def median_flow(hydrogram: list) -> tuple:
+#     hist = {}
+#     for d in hydrogram:
+#         bucket = int(d[1])
+#         hist[bucket] = hist.get(bucket, 0) + 1
 
-    l = sum(hist.values()) / 2
-    c = 0
-    for b in sorted(hist.keys()):
-        if c + hist[b] > l:
-            return b, int(hydrogram[c][0])
+#     l = sum(hist.values()) / 2
+#     c = 0
+#     for b in sorted(hist.keys()):
+#         if c + hist[b] > l:
+#             return b, int(hydrogram[c][0])
 
-        c += hist[b]
+#         c += hist[b]
 
-    return 0, 0
+#     return 0, 0
+
+def hydrogram_statistics(hydrogram):
+    time = 0
+    flood = 0
+    peak = 0
+    mean = 0
+    flow = []
+
+    delay = 0
+    for t, f in hydrogram:
+        t = int(t) - delay
+        f = float(f)
+        if f == 0:
+            delay += t
+            continue
+        mean = (mean * time + f * t) / (time + t)
+        time += t
+        flood += f * t
+        peak = max(peak, f)
+        flow += [f] * t
+
+    return {
+        "time": time,
+        "flood": flood,
+        "peak": peak,
+        "mean": mean,
+        "median": list(sorted(flow))[int(len(flow) / 2)]
+    }
 
 def gen_hydrogram(
     hydrogram: list
